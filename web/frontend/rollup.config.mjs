87bfb69cb9f0754cc4ac013507b0da60ab0e00eb
Kelavath Balaji Naik <101827986+Balaji91221@users.noreply.github.com>
2024-01-23 07:33:39 +0000

    Modified to dev env

--- web/frontend/rollup.config.mjs
+++ web/frontend/rollup.config.mjs
@@ -4,6 +4,7 @@ import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
 import terser from '@rollup/plugin-terser';
 import css from 'rollup-plugin-css-only';
+import livereload  from 'rollup-plugin-livereload'; 
 
 // const production = !process.env.ROLLUP_WATCH;
 const production = false
@@ -51,6 +52,7 @@ const entrypoint = (name, path) => ({
         // we'll extract any component CSS out into
         // a separate file - better for performance
         css({ output: `${name}.css` }),
+        livereload("public")
     ],
     watch: {
         clearScreen: false
